d54e3771863553bd796a456dfe6da85f64c2966e
Diegoteman <diegocasalta@gmail.com>
2023-10-29 17:13:26 +0100

    hdbkajnldwa

--- Scenes/Body.gd
+++ Scenes/Body.gd
@@ -23,8 +23,10 @@ func _input(event):
 	if Input.is_action_just_pressed("Haunt") && playerIn: Possess()
 
 func Possess():
+	$"../Nun/Camera2D".make_current()
 	Prompt(true)
 	GD.Player.canMove = false
+	GD.Player.Sprite.flip_h = true
 	UI.Close()
 	var t = create_tween()
 	t.tween_property(GD.Player, "modulate:a", 0, 2)
@@ -38,4 +40,27 @@ func Possess():
 	T.start()
 	await T.timeout
 	$"../Nun".Die()
+	T.start()
+	await T.timeout
+	t = create_tween()
+	t.tween_property(GD.Player, "modulate:a", 1, 1)
+	await t.finished
+	GD.Player.Scare()
+	T.wait_time = 3.3
+	T.start()
+	await T.timeout
+	t = create_tween().set_trans(Tween.TRANS_SINE).set_parallel()
+	t.tween_property(GD.Player, "position", $"../Nun/Camera2D".global_position, 1)
+	t.tween_property($"../ColorRect2", "color:a", 1, 1)
+	await t.finished
+	GD.Player.Sprite.visible = false
+	$"../Nun/Camera2D/Sprite2D".visible = true
+	$"../Nun/Camera2D/Sprite2D/AnimationPlayer".play("ascend")
+	t = create_tween().set_trans(Tween.TRANS_SINE).set_parallel()
+	t.tween_property($"../Nun/Camera2D/Sprite2D", "position.y", position.y - 100, 8)
+	t.tween_property($"../CharacterBody2D/PointLight2D2", "energy", 2, 8)
+	t.tween_property($"../Nun/Camera2D/Sprite2D", "modulate.a", 0, 8)
+	await t.finished
+	$"../ColorRect3".visible = true
+	$"../CharacterBody2D/PointLight2D2".queue_free()
 	
--- Scenes/nun.gd
+++ Scenes/nun.gd
@@ -5,28 +5,18 @@ var dead = false
 func Die():
 	if dead: return
 	dead = true
-	$NPC/AnimationPlayer.play("Scare")
-	GD.Player.Scare()
-	for i in range(120):
+	$NPC.visible = false
+	$Sprite2D.visible = true
+	$Sprite2D/AnimationPlayer.play("scare")
+	for i in range(240):
 		$NPC.position = Vector2(randi_range(-3,3), randi_range(-3,3)) * i / 60
 		await get_tree().physics_frame
 	$NPC.self_modulate = Color.TRANSPARENT
 	for i in range(20):
-		$Camera2D.offset = Vector2(randi_range(-10, 10), randi_range(-10, 10))
+		$Camera2D.offset = Vector2(randi_range(-2, 2), randi_range(-2, 2))
 		await get_tree().physics_frame
-	$Timer.start()
-	await $Timer.timeout
-	GD.Player.get_node("Camera2D").make_current()
-	GD.Player.canMove = true
+	$Sprite2D.visible = false
+	$Sprite2D2.visible = true
+	var t = create_tween()
+	t.tween_property($Sprite2D2, "scale", Vector2.ONE*2, .5).from(Vector2(1.5, .8)*2).set_trans(Tween.TRANS_SINE)
 	UI.Open()
-	get_node("../Talk").queue_free()
-	get_node("../Talk4").monitorable = true
-
-func Trigger():
-	get_node("../Talk").queue_free()
-	get_node("../Talk4").monitorable = true
-	if GD.peaDead && GD.femboyDead: 
-		get_node("../Trigger3").monitoring = true
-		get_node("../Trigger3/Sprite2D2").visible = true
-		get_node("../Nun").queue_free()
-	queue_free()
